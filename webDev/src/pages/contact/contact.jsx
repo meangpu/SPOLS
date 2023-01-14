5f3341a06672041fb3de4e240fd9084262a1656e
meangpu <51825872+meangpu@users.noreply.github.com>
2023-01-14 10:59:23 +0700

    add react web dev

---
+++ webDev/src/pages/contact/contact.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./contact.css";
+
+const contact = () => {
+  return <section className='container'>contact</section>;
+};
+
+export default contact;
---
+++ webDev/src/pages/job/Job.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./job.css";
+
+const Job = () => {
+  return <section className='container'>Job</section>;
+};
+
+export default Job;
