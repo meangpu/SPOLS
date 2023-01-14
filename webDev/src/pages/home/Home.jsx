0a8e24e89fc9523ca38bf8daea50f2d21a1e214d
meangpu <51825872+meangpu@users.noreply.github.com>
2023-01-14 11:02:29 +0700

    delete website

--- webDev/src/pages/home/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Home.css";
-
-const Home = () => {
-  return <section className='container'>HOME</section>;
-};
-
-export default Home;
--- webDev/src/pages/notFound/Notfound.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Notfound.css";
-const Notfound = () => {
-  return <div>Notfound</div>;
-};
-
-export default Notfound;
