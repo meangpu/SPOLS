56e0c482484066d9fc085096f88d96a667965acc
meangpu <51825872+meangpu@users.noreply.github.com>
2023-01-15 19:23:40 +0700

    update icon

--- webDev/src/components/navbar/Navbar.jsx
+++ webDev/src/components/navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "../../img/icon.png";
+import logo from "../../img/logo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className='main-nav'>
       <div className='container nav__container'>
-        <a to='/' className='logo '>
+        <a href='https://meangpu.github.io/' className='logo '>
           <img width='64' height='64' src={logo} alt='meangpu website' />
         </a>
       </div>
