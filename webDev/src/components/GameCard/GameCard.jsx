fde590353da3191813af185f64ff61a71fddf6f6
meangpu <51825872+meangpu@users.noreply.github.com>
2023-01-15 22:16:43 +0700

    add autoplay for ios

--- webDev/src/components/GameCard/GameCard.jsx
+++ webDev/src/components/GameCard/GameCard.jsx
@@ -20,7 +20,7 @@ const GameCard = (props) => {
 
         {props.gif && (
           <div className='game_card__item-image active '>
-            <video loop autoPlay muted className='video'>
+            <video loop autoPlay muted playsinline className='video'>
               <source
                 src={props.gif}
                 type='video/mp4'
